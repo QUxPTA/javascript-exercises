b4224d0b8483e88f81199d89a0a8855e419f52fb
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-05-23 17:52:37 +0300

    Implement repeatString function to concatenate a string a specified number of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+  if (times <= 0) return 'Something Went Wrong';
+  let string = '';
+  for (let i = 0; i < times; i++) {
+    string += str;
+  }
+  return string;
 };
 
 // Do not edit below this line
