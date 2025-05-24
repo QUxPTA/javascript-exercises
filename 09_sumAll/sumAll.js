266ae3b65171888fedd9932727a10bd0caa09933
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-05-24 18:57:13 +0300

    Implement sumAll function with input validation and enable previously skipped tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  // Swap if greater number starts
+  if (start > end) {
+    let temp = start;
+    start = end;
+    end = temp;
+  }
+  // Check for negative numbers
+  if (start < 0 || end < 0) {
+    return 'ERROR';
+  }
+  // Check if not a number or float
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return 'ERROR';
+  }
+  let sum = 0;
 
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
