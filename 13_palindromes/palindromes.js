7d350d3ff395bbfb61c5d63c09fe0430e2ca2e46
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-06-06 10:56:40 +0300

    Refactor palindromes function to accept a string parameter and enable previously skipped tests
    The function now checks if a string is a palindrome, and handles edge cases such as strings with pucntuations

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Characters to consider
+  const alphanumerals = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerals.includes(character))
+    .join('');
+
+  const reversedString = cleanedString.split('').reverse().join('');
+  return reversedString === cleanedString;
 };
 
 // Do not edit below this line
