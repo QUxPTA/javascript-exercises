15dfc8f4651f13237879a2212004a33ae5be33ad
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-06-07 09:43:03 +0300

    Add Fibonacci gunctionality and enable previously skipped tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let number = Number(n);
+  if (number < 0) {
+    return 'OOPS';
+  }
 
+  if (number <= 1) {
+    return number;
+  }
+  return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
