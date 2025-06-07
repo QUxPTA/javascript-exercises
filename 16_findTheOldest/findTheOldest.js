06e00e70121f5b2558f6155ad875ea42c3ab3fb2
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-06-07 10:45:42 +0300

    Complete findTheOldest function to meet all expectations,  and enable skipped tests.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // Get the current year, for purposes of calculating age where person is still alive
+  const currentYear = new Date().getFullYear();
+  // Get the age of the first person in the array
+  let oldestPerson = people[0];
+  let oldestPersonAge =
+    (oldestPerson.yearOfDeath ?? currentYear) - oldestPerson.yearOfBirth;
 
+  // Loop through people, checking if there is a person older than people[0]
+  for (const person of people) {
+    const deathYear = person.yearOfDeath ?? currentYear;
+    const age = deathYear - person.yearOfBirth;
+    if (age > oldestPersonAge) {
+      oldestPersonAge = age;
+      oldestPerson = person;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
